c15e275c28a879f0bb36590078f0e630abba68b0
Ali zaib <alizaib12220@gmail.com>
2024-03-05 18:38:38 +0500

    Advance  php oop challenges

--- oop.php
+++ oop.php
@@ -224,7 +224,84 @@ $book->displayDetails();
 
 $dvd = new DVD("The Land Before Time", "Charles Grosvenor", 2010, 150);
 $dvd->displayDetails();
+
+
+// ***********PHP class for student information*********
+
+
+
+class Student {
+    public $name;
+    public $age;
+    public $grade;
+
+    public function displayInfo() {
+        echo "Name: " . $this->name . "</br>";
+        echo "Age: " . $this->age . "</br>";
+        echo "Grade: " . $this->grade . "</br>";
+    }
+}
+
+$student = new Student();
+$student->name = "Gwladus Andrea";
+$student->age = 16;
+$student->grade = 10;
+$student->displayInfo();
+
+
+// ***********PHP class for bank accounts: Deposit and withdraw***********
+
+
+
+class BankAccount {
+    private $accountNumber;
+    private $balance;
+
+    public function __construct($accountNumber) {
+        $this->accountNumber = $accountNumber;
+        $this->balance = 0;
+    }
+
+    public function getAccountNumber() {
+        return $this->accountNumber;
+    }
+
+    public function getBalance() {
+        return $this->balance;
+    }
+
+    public function deposit($amount) {
+        $this->balance += $amount;
+        echo "Deposited $amount into account $this->accountNumber. New balance: $this->balance</br>";
+    }
+
+    public function withdraw($amount) {
+        if ($this->balance >= $amount) {
+            $this->balance -= $amount;
+            echo "Withdrawn $amount from account $this->accountNumber. New balance: $this->balance</br>";
+        }
+        else
+        {
+           echo "Want to withdrawn $amount?</br>";
+           echo "Insufficient balance in account $this->accountNumber. Current balance: $this->balance</br>";
+        }
+    }
+}
+
+$account = new BankAccount("SB-1234");
+echo "Account Number: " . $account->getAccountNumber() . "</br>";
+echo "Initial Balance: " . $account->getBalance() . "</br>";
+
+$account->deposit(1000);
+$account->withdraw(600);
+$account->withdraw(700);
+
+
+
 ?>
+
+
+
 
 
 
